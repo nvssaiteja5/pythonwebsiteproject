e37b5d254133106ab010543400cc9b434db42e27
jogubaba <baba.jogu510@gmail.com>
2021-04-21 21:08:21 +0530

    10/10

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
+# WEBPAGE CERTIFICATE EXPIRY CHECK
+
+""" This is a script to check the Web Page Certificate Expiry Date
+and to send an email to user 10 days before the Date of Expiry"""
+
 import smtplib
-import email, getpass, imaplib, os
-import whois
+import getpass
 from datetime import date
+import whois
 
-#create a "o" object of whois class
-webadd=input("enter the web address")
-o= whois.whois(webadd)
-web_expirydate=o.expiration_date
+# Create a "ADDRESS" object of whois class
+WEBADD = input("Enter the Web Address : ")
+ADDRESS = whois. query(WEBADD)
+WEB_EXPIRYDATE = ADDRESS. expiration_date
 
-
-#to get present date 
-today = date.today()
+# To get present date
+TODAY = date. TODAY()
 # dd/mm/YY
-presentdate = today.strftime("%d/%m/%Y")
+PRESENTDATE = TODAY. strftime("%d/%m/%Y")
 #print("d1 =", d1)
 
-
-
-
-#to get gmail id and password
-user = raw_input("Enter your GMail username:")
-pwd = getpass.getpass("Enter your password: ")
-a= smtplib.SMTP_SSL("smtp.gmail.com",465)
-a.login(user,pwd)
+# To get gmail id and password
+USER = input("Enter your GMail username : ")
+PWD = getpass. getpass("Enter your password : ")
+EMAIL = smtplib. SMTP_SSL("smtp.gmail.com", 465)
+EMAIL. login(USER, PWD)
 #presentdate=20
 #web_expirydate=15
 
-if(presentdate-web_expirydate<10):
-  a.sendmail(user,user,"Your website is going to expiry")
-a.quit()
+if PRESENTDATE-WEB_EXPIRYDATE < 10:
+    EMAIL. sendmail(USER, USER, "Your website is going to expiry")
+EMAIL. quit()
