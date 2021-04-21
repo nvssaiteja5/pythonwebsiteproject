6947c40aa80485b80ed1452be3525e8775c8bb92
nvssaiteja5 <82872227+nvssaiteja5@users.noreply.github.com>
2021-04-21 08:55:10 +0530

    Update gmaiil.py

--- gmaiil.py
+++ gmaiil.py
@@ -1,6 +1,12 @@
 import smtplib
+import email, getpass, imaplib, os
 
+user = raw_input("Enter your GMail username:")
+pwd = getpass.getpass("Enter your password: ")
 a= smtplib.SMTP_SSL("smtp.gmail.com",465)
-a.login("sender","password")
-a.sendmail('sender@gmail.com','reciver@gmail.com',"Your website is going to expiry ")
+a.login(user,pwd)
+presentdate=20
+web_expirydate=15
+if(presentdate-web_expirydate<10):
+  a.sendmail(user,user,"Your website is going to expiry")
 a.quit()
