4493e5bea6bbd78497feeb9e23154b9879b46e10
nvssaiteja5 <82872227+nvssaiteja5@users.noreply.github.com>
2021-04-22 17:35:24 +0530

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -1,11 +1,11 @@
 pylint $1 > result
 
-SCORE=`cat result | grep rated|awk  '{print $7}' |awk -F/ '{print $1}'`
+SCORE=`cat result | grep rated|awk  '{print $7}' |awk -F/ '{print $1}'|awk -F. '{print $1}'`
 echo $SCORE
-if [[ $SCORE > 5.00 ]]
+if [ $SCORE -gt 10 ]
 then
 echo "Success"
-else 
+else
 echo "Score is $SCORE, Score must be  10"
 exit 1
 fi
